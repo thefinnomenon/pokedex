2790958d50b81c5562e38ccb4293a525b7c48265
Chris Finn <christopherpatrickfinn@gmail.com>
2016-03-01 10:14:47 -0500

    Created Pokemon model, PokeCell view, & initial UI stuff

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Chris Finn on 3/1/16.
+//  Copyright © 2016 Crispy Finnovations. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalizedString
+        thumbImage.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Chris Finn on 3/1/16.
+//  Copyright © 2016 Crispy Finnovations. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
